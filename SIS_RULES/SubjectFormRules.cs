d75f3fecd26f04045cf6a089ec8c7ec3ab97c5db
AngelNacor <116412052+AngelNacor@users.noreply.github.com>
2023-07-07 23:38:07 +0800

    subject code and description rules

---
+++ SIS_RULES/SubjectFormRules.cs
@@ -0,0 +1,47 @@
+using System;
+using SIS_UI;
+using SIS_MODEL;
+using SIS_DATA;
+using AM_Rules; 
+namespace SIS_RULES
+{ 
+    public class SubjectCodeDescription
+    {
+        public static void studentSubjectCodeDescription()
+        {
+            int SubjectCodeDescription;
+
+            do
+            {
+                SubjectCodeDescription = SubjectForm.ViewFirstYearFirstSemester() , SubjectForm.ViewFirstYearSecondSemester() , SubjectForm.ViewSecondYearFirstSemester() ,
+                SubjectForm.ViewSecondYearSecondSemester();
+
+                    switch (SubjectCodeDescription)
+                    {
+                        case 0: break;
+                        case 1: //overall subject code description
+                        SubjectForm.ViewFirstYearFirstSemester(), 
+                        SubjectForm.ViewFirstYearSecondSemester();
+                        SubjectForm.ViewSecondYearFirstSemester();
+                        SubjectForm.ViewSecondYearSecondSemester();
+                        break;
+                        case 2: //first year first semester
+                        SubjectForm.ViewFirstYearFirstSemester();
+                        break;
+                        case 3: //first year second semester
+                        SubjectForm.ViewFirstYearSecondSemester();
+                        break;
+                        case 4: //second year first semester
+                        SubjectForm.ViewSecondYearFirstSemester();
+                        break;
+                        case 5: //second year second semester
+                        SubjectForm.ViewSecondYearSecondSemester();
+                        break;
+                        while(SubjectCodeDescription == null);
+
+                    }
+            }
+        }
+    }
+}
+
